1cb97924755647da2d05d01eba0634fa302ad620
zee <zperezcanals@gmail.com>
2018-05-17 22:08:19 -0400

    new folder structure

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,55 @@
+class StudentsController < ApplicationController
+
+
+	def index
+		puts "i am in path /students"
+		@student = User.where(role: 'Student')
+		@user_stud = Student.all
+	end
+
+
+	def  show
+		puts "I am in /students/id route"
+		@student =User.where(role: 'Student')
+	end
+    
+    def new
+		@student =User.new	
+	end
+
+	def create
+		@student = Student.create(student_params)
+		redirect_to '/students'
+	end
+
+
+    def edit 
+        @student = User.find(params[:id])
+    end
+    
+
+    def update
+        @student = User.find(params[:id])
+        @student.update(student_params)
+        redirect_to '/students'
+    end
+
+
+    def destroy
+        User.find(params[:id]).destroy
+        redirect_to '/students'
+    end
+
+
+	private
+    # Using a private method to encapsulate the permissible parameters is
+    # just a good pattern since you'll be able to reuse the same permit
+    # list between create and update. Also, you can specialize this method
+    # with per-user checking of permissible attributes.
+    # def user_params
+    #   params.require(:user).permit(:first_name, :last_name, :email , :password, :age, :degree, :role, :avatar)
+    # end
+    def student_params
+        params.require(:student).permit(:user_id, :cohort_id, :grade)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/assigns/students/:id", to: "assigns#new"
+  get "/assigns/instructors/:id", to: "assigns#new"
+  get "/assigns/students/:id/edit", to: "assigns#edit"
+  get "/assigns/instructors/:id/edit", to: "assigns#edit"
+  post "/assigns/students/:id", to: "assigns#update"
+  post "/assigns/instructors/:id", to: "assigns#update"
+
+  resources :home
+    get "/home/login", to: "home#login"
+    post "/home/login", to: "home#login"
+  resources :users
+  resources :students
+  resources :instructors
+  resources :courses
+  resources :cohorts
+end
